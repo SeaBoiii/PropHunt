74cd0aeb13449bc922f4ae85f5d741b42e7ca3a7
TomSki <tomaszwojciechowski@hotmail.co.uk>
2013-09-03 22:09:18 +0100

    Add Shop.yml, add shopConfig

---
+++ src/main/java/me/tomski/prophuntstorage/ShopConfig.java
@@ -0,0 +1,73 @@
+package me.tomski.prophuntstorage;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+
+import me.tomski.arenas.Arena;
+import me.tomski.objects.ArenaFileStructureWrapper;
+import me.tomski.objects.LocationBox;
+import me.tomski.prophunt.DisguiseManager;
+import me.tomski.prophunt.GameManager;
+import me.tomski.prophunt.PropHunt;
+import me.tomski.arenas.ArenaConfig;
+import me.tomski.arenas.ArenaManager;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+
+public class ShopConfig {
+
+
+    public FileConfiguration StorageFilef = null;
+    private File customConfigFile = null;
+    private PropHunt plugin;
+
+    public ShopConfig(PropHunt plugin) {
+        this.plugin = plugin;
+    }
+
+
+    public void loadData() {
+
+    }
+
+    public void reloadShopConfig() {
+        if (customConfigFile == null) {
+            customConfigFile = new File(plugin.getDataFolder(), "Shop.yml");
+        }
+        StorageFilef = YamlConfiguration.loadConfiguration(customConfigFile);
+
+        // Look for defaults in the jar
+        InputStream defConfigStream = plugin.getResource("Shop.yml");
+        if (defConfigStream != null) {
+            YamlConfiguration defConfig = YamlConfiguration
+                    .loadConfiguration(defConfigStream);
+            StorageFilef.setDefaults(defConfig);
+        }
+    }
+
+    public FileConfiguration getShopConfig() {
+        if (StorageFilef == null) {
+            this.reloadShopConfig();
+        }
+        return StorageFilef;
+    }
+
+    public void saveShopConfig() {
+        if (StorageFilef == null || customConfigFile == null) {
+            return;
+        }
+        try {
+            getShopConfig().save(customConfigFile);
+        } catch (IOException ex) {
+            plugin.getLogger().log(Level.SEVERE,
+                    "Could not save config to " + customConfigFile, ex);
+        }
+    }
+
+}
